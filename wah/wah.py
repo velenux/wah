6915a803a0b750853c94510aa18c02b127778ebf
Gilberto Ficara <g.ficara@stardata.it>
2017-04-23 09:58:39 +0200

    Added a proper card-add routine. Added mock login/logout.

--- wah/wah.py
+++ wah/wah.py
@@ -1,13 +1,16 @@
 # encoding: utf-8
 
-from flask import Flask
+from flask import *
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///wah.sqlite'
 db = SQLAlchemy(app)
+app.secret_key = 'FIXME: Change Me'
+
 
 class Card(db.Model):
+    """Holds a card's data."""
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(512), unique=True)
 
@@ -18,20 +21,58 @@ class Card(db.Model):
         return '"%r"' % self.text
 
 
-# create the database if it doesn't exist
 try:
     cards = Card.query.first()
+    print('Database ready.')
 except Exception as e:
     db.create_all()
+    print('Initialized the database.')
 
 
 @app.route('/')
-def show_index():
-    return 'Currently holding %d cards' % db.session.query(Card).count()
+def show_main_index():
+    """Shows the main index for the website."""
+    return render_template('main_index.html', card_number = db.session.query(Card).count())
 
-@app.route('/card/add')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Logs the user in the website."""
+    if request.method == 'POST':
+        error = None
+        # FIXME: stub
+        if request.form['username'] == 'donald.trump':
+            error = 'Invalid account'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_main_index'))
+    # if method is not POST
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    """Logs the user out of the website."""
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_main_index'))
+
+
+@app.route('/card/add', methods=['GET', 'POST'])
 def add_card():
-    c = Card('sopra la panca la capra canta')
-    db.session.add(c)
-    db.session.commit()
-    return 'Added.'
+    """Tries to add a new card to the database."""
+    if request.method == 'POST':
+        error = None
+        try:
+            c = Card(request.form['card-text'])
+            db.session.add(c)
+            db.session.commit()
+            flash('Card added!')
+            return redirect(url_for('show_main_index'))
+        except Exception as e:
+            flash('Error adding the card!')
+            error = str(e)
+            return render_template('login.html', error=error)
+    # if method is not POST
+    return render_template('login.html')
